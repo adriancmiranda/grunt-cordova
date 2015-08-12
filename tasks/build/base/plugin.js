7ef8337c5a41af613e86d9fda70c47c08de7279e
Adrian Miranda <adriancmiranda@gmail.com>
2015-08-12 11:32:55 -0300

    Update plugin.js

--- tasks/build/base/plugin.js
+++ tasks/build/base/plugin.js
@@ -11,12 +11,23 @@
     var addPlugin, helpers;
     helpers = require('../../helpers')(grunt);
     addPlugin = function(plugin, fn) {
-      var cmd, uri;
+      var cmd, uri, key, vars = '';
       uri = new URI(plugin);
       if (uri.protocol() === '' && (plugin.substr(0, 1) === '.' || plugin.substr(0, 1) === '/')) {
         plugin = path.resolve(uri.path());
       }
-      cmd = "cordova plugin add " + plugin + " " + (helpers.setVerbosity());
+      if(({}.toString).call(plugin).replace(/^(\[object(\s|\uFEFF|\xA0))|(\])$/g, '').toLowerCase() === 'object'){
+        if(plugin.vars){
+          for(key in plugin.vars){
+            vars += ' --variable '+key+'='plugin.vars[key];
+          }
+        }
+        if(plugin.url){
+          cmd = "cordova plugin add " + plugin.url + vars + " " + (helpers.setVerbosity());
+        }
+      }else{
+        cmd = "cordova plugin add " + plugin + " " + (helpers.setVerbosity());
+      }
       return helpers.exec(cmd, fn);
     };
     return {
